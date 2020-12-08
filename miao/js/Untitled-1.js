eea1c4fa8a398f58a73716a77167cdeae9ee44b4
wuchengliang <924507542@qq.com>
2020-12-08 16:20:07 +0800

    lodash

---
+++ miao/js/Untitled-1.js
@@ -0,0 +1,14 @@
+<script>
+  var a = 1
+  var b = 3
+  var c = 99
+  var d = 12
+  var e = 8
+  var f = 0
+  a * x + b * y = c
+  d * x + c * y = f
+  var x = (c * e - b * f) / (a * e - b * d)
+  var y = (c * d - a * f) / (b * d - a * e)
+  console.log(x)
+  console.log(y)
+</script>
---
+++ miao/lodash/wuchengliang-lodash.js
@@ -0,0 +1,16 @@
+var wuchengliang = function () {
+  function compact(ary) {
+    var result = []
+    for (var i = 0; i < ary.length; i++) {
+      if (ary[i]) {
+        result.push(ary[i])
+      }
+    }
+  }
+
+
+  return result
+
+
+
+}()
