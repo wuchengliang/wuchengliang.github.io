843c4d65f0145e7e13a7d583f1d580b2b8a2a9a2
wuchengliang <924507542@qq.com>
2020-12-09 11:00:38 +0800

    10.9

--- miao/lodash/wuchengliang-lodash.js
+++ miao/lodash/wuchengliang-lodash.js
@@ -9,10 +9,65 @@ var wuchengliang = function () {
     }
     return result
   }
+  function chunk(ary, size) {
+    var result = []
+    for (i = 0; i < ary.length; i = i + size) {
+      if (i + size < ary.length) {
+        result.push(ary.slice(i, i + size))
+      }
+      else {
+        result.push(ary.slice(i, ary.length))
+      }
+    }
+    return result
+  }
+  function join(ary, separator) {
+    var result = ""
+    for (var i = 0; i < ary.length - 1; i++) {
+      result = result + ary[i] + separator
+    }
+    return result + ary[ary.length - 1]
+  }
+  function last(ary) {
+    return ary[ary.length - 1]
+  }
+  function lastIndexOf(ary, value, fromIndex = ary.length - 1) {
+    for (var i = fromIndex; i >= 0; i--) {
+      if (ary[i] == value) {
+        return i
+      }
+    }
+  }
+  function drop(ary, n = 1) {
+    ary.splice(0, n)
+    return ary
+  }
+  function dropRight(ary, n = 1) {
+    ary.splice(ary.length - n - 1, n)
+    return ary
+  }
+  function fill(ary, value, start = 0, end = ary.length) {
+    for (var i = start; i < end; i++) {
+      ary[i] = value
+    }
+    return ary
+  }
+
+
 
 
   return {
     compact,
+    chunk,
+    join,
+    last,
+    lastIndexOf,
+    drop,
+    dropRight,
+    fill,
+
+
+
 
   }
 
